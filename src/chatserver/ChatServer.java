78d2f0fe25f0b1bfb5488ce8a979b92a30130660
z1mmz <wolfz@live.com>
2015-09-21 12:53:16 +1000

    first

---
+++ src/chatserver/ChatServer.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2015 wolf
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package chatserver;
+
+/**
+ *
+ * @author wolf
+ */
+import java.io.*;
+import java.net.*;
+
+import java.util.Scanner;
+
+public class ChatServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try{
+        ServerSocket serverSocket = new ServerSocket(9980);
+        Socket clientSocket = serverSocket.accept();
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
+        Scanner in = new Scanner(clientSocket.getInputStream());
+        out.println("Welcome to chat server 1");
+        while(true){
+            
+            if(in.hasNext()){
+                System.out.println(in.nextLine());
+                    
+            }
+            
+        }
+        } catch (Exception e){
+        }
+        }   
+}
+    
